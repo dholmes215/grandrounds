b695eabdb62a4998bd5ec072a44f5369b58edf58
David Holmes <dholmes@dholmes.us>
2022-04-11 21:23:29 -0500

    Add range/v3/all.hpp header to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
-﻿
+﻿#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 4242)
+#pragma warning(disable : 4702)
+#endif
+#include <range/v3/all.hpp>
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
+
 int main(int /* argc*/, const char** /* argv*/)
 {
 }
