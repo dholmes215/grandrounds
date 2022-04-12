da2dd865d273620af4c300bac67bdaf04b933a6b
David Holmes <dholmes@dholmes.us>
2022-04-12 09:14:08 -0500

    Remove warning suppression

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,4 @@
-﻿#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning(disable : 4242)
-#pragma warning(disable : 4702)
-#endif
-#include <range/v3/all.hpp>
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
+﻿#include <range/v3/all.hpp>
 
 int main(int /* argc*/, const char** /* argv*/)
 {
